511a906dd00545d28a0352a62b7c84af65ababa3
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-10-20 01:07:09 -0300

    Test Model

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -1,29 +1,51 @@
 const { expect } = require("chai");
-const sinon = require('sinon');
+const sinon = require("sinon");
 
 const connection = require("../../../src/db/connection");
 const productModel = require("../../../src/models/productModel");
 
-const productsMock = require('../mocks/products.mock')
+const productsMock = require("../mocks/products.mock");
 
-describe('Model de produtos', function () {
-  describe('Listar todos os produtos', function () {
-    productsMock;
+describe("TESTANDO A CAMADA MODEL", function () {
+  describe("Listar os produtos", function () {
 
-    beforeEach(() => {
+    it("Deve retornar um array com todos os elementos getByProductsData(22-28)", async function () {
+      // Arranjo
       sinon.stub(connection, "execute").resolves([productsMock]);
-    });
 
-    it('Deve retornar um array com todos os elementos', async function () {
+      // Ação
       const result = await productModel.getByProductsData();
 
-      expect(result).to.be.a('array');
-      expect(result).to.be.deep.eq(productsMock);
+      // Assertiva
+      expect(result).to.be.deep.equal(productsMock);
     });
 
-    afterEach(() => {
-      sinon.restore;
+    it("Deve retornar um array com um elemento findByID(30-36)", async function () {
+      // Arranjo
+      sinon.stub(connection, "execute").resolves([productsMock]);
+
+      // Ação
+      const result = await productModel.findById([productsMock]);
+      console.log('O result da MOdel é: ', result);
+
+      // Assertiva
+      expect([result[0]]).to.be.deep.eq([{ id: 1, name: "Martelo de Thor" }]);
     });
-  })
+
+    describe("Inserir Produtos na Model(5-20)", () => {
+      it("Deve inserir um array com novo produto", async function () {
+        // Arranjo
+        sinon.stub(connection, "execute").resolves({id: 4, name: "Capa do Sr Destino"});
+
+        // Ação
+        const result = await productModel.insert({name: "Capa do Sr Destino"});
+
+        // Assertiva
+        // expect(result).to.be.a("array");
+        expect(result).to.be.deep.eq({ id: 4, name: "Capa do Sr Destino" });
+      });
+    });
+
+    afterEach(sinon.restore);
+  });
 });
-
